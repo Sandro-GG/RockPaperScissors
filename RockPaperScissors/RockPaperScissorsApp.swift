3a8f729f5053da038210a4c4436948cb355bc8fe
Sandro Gakharia <sandrogakharia@gmail.com>
2025-07-08 11:59:47 +0400

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Sandro Gakharia on 08.07.25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsTests.swift
+//  RockPaperScissorsTests
+//
+//  Created by Sandro Gakharia on 08.07.25.
+//
+
+import Testing
+@testable import RockPaperScissors
+
+struct RockPaperScissorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
